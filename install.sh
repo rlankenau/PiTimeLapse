625b96d13983417c9dedc18d1081f33baaa94012
Russ Lankenau <rlankenau@gmail.com>
2015-05-26 03:37:20 +0000

    Updated init.d scripts and install to use chkconfig

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ cp ./monitor.py /usr/sbin/rpi-photo-monitor.py
 chmod u+x /usr/sbin/rpi-photo-monitor.py
 cp ./init.d/copy /etc/init.d/rpi-photo-copy
 cp ./init.d/monitor /etc/init.d/rpi-photo-monitor
+
+#try to enable through chkconfig
+chkconfig -a rpi-photo-copy
+chkconfig -a rpi-photo-monitor
