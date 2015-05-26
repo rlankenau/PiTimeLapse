3631669cd13abe348e6f7a51be3a342819717704
Russ Lankenau <rlankenau@gmail.com>
2015-05-26 01:37:38 +0000

    Updated pins to use BCM numbering.  Added start-time based tag.

--- monitor.py
+++ monitor.py
@@ -15,18 +15,22 @@ cam.led = False
 cam.vflip = True
 time.sleep(2)
 
+tag = time.time()
+
 
 def capture_frame(r, f):
 	global tag
 	global cam
-	cam.capture('/var/www/{2}-frame{0:03d}{1:08d}.jpg'.format(r,f,tag))
+	cam.capture('/var/photos/{2}-frame{0:03d}{1:08d}.jpg'.format(r,f,tag))
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT)
-GPIO.setup(11, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+GPIO.output(18, False)
+GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 
 def buttonPress(channel):
+	print "Button pressed."
 	global running
 	global run
 	global frame
@@ -37,16 +41,17 @@ def buttonPress(channel):
 		frame = 0;
 		running = True
 
-GPIO.add_event_detect(11, GPIO.RISING, callback=buttonPress, bouncetime=2000)
+GPIO.add_event_detect(17, GPIO.RISING, callback=buttonPress, bouncetime=2000)
+print "Ready for input"
 
 while True:
 	start = time.time()
 	if running:
 		started = True
 		capture_frame(run,frame)
-		GPIO.output(12, True)
+		GPIO.output(18, True)
 		time.sleep(.5)
-		GPIO.output(12, False)
+		GPIO.output(18, False)
 		frame += 1
 	elif started == True:
 		started = False
